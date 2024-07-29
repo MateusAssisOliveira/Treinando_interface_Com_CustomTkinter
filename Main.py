1dd5e0f8a4fad33a68bfc485c363f2f695350a9e
Mateus <queromais.uba@gmail.com>
2024-07-29 19:12:13 -0300

    Aprendedno sobre checkboxes e classes

--- Main.py
+++ Main.py
@@ -52,9 +52,30 @@ button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 
 #Add checkboxes -> Adicionar caixas de seleção
 
+#Para tornar o layout um pouco mais complexo, adicionamos duas caixas de seleção na segunda linha.
+
+checkbox_1 = customtkinter.CTkCheckBox(app, text="checkbox 1")
+checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+
+checkbox_2 = customtkinter.CTkCheckBox(app, text="checkbox 2")
+checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
 
 
+# Note que o argumento pady obtém uma tupla, o que significa 0 padding na parte superior e 20 padding na parte inferior. 
+# Note também que as caixas de seleção são fixas apenas no lado oeste de sua célula de grade. 
+# Para deixar o botão abranger a janela inteira novamente, precisamos definir um columnspan de 2 para ele (use rowspan para linhas).
 
+button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+
+# E para espaçar igualmente as caixas de seleção, também precisamos dar a ambas as colunas um peso igual.
+
+app.grid_columnconfigure((0, 1), weight=1)
+
+# Usando classes
+#Para completar esta introdução, queremos estruturar este programa em uma classe. 
+# É altamente recomendado usar classes que herdam de CTk para a janela principal ou CTkFrame para um quadro, 
+# porque isso aumenta muito a legibilidade do código e o código se torna facilmente extensível, 
+# porque as classes podem ser facilmente distribuídas para vários arquivos.
 
 
 
