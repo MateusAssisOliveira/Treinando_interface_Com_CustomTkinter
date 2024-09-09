0d503714dfe1ebe69873b069f30145d40251bae2
Mateus <queromais.uba@gmail.com>
2024-09-09 19:16:23 -0300

    Aprendendo mais sobre CTkFrame e suas funcionalidade

--- Using_classes/Using_classes.py
+++ Using_classes/Using_classes.py
@@ -17,7 +17,23 @@ class MyCheckboxFrame(customtkinter.CTkFrame):
         
         self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
         self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-
+        
+        self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        
+    def get(self):
+        
+        checked_checkboxes = []
+        
+        if self.checkbox_1.get() == 1:
+            
+            checked_checkboxes.append(self.checkbox_1.cget("text"))
+        if self.checkbox_2.get() == 1:
+            checked_checkboxes.append(self.checkbox_2.cget("text"))
+        if self.checkbox_3.get() == 1:
+            checked_checkboxes.append(self.checkbox_3.cget("text"))
+        return checked_checkboxes
+        
 class App(customtkinter.CTk):
     
     def __init__(self):
@@ -36,7 +52,8 @@ class App(customtkinter.CTk):
         self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
     def button_callback(self):
-        print("button pressed")
+        print("checked checkboxes:", self.checkbox_frame.get())
+
 
 app = App()
 app.mainloop()
